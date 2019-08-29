30331cd78b48e172357e9534fd1671eb3f4f03d8
Nemrosim <nemrosim1988@gmail.com>
2019-08-29 21:31:44 +0300

    changed root path.  created /root and /about pages in alpha-blog-before project

--- rails-projects/alpha-blog-before/config/routes.rb
+++ rails-projects/alpha-blog-before/config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # URL 'welcome/home';
+  # route to 'welcome' controller;
+  # 'home' action;
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+
+  # V1
+  # get 'welcome/home', to: 'welcome#home'
+  # get 'welcome/about', to: 'welcome#about'
 end
